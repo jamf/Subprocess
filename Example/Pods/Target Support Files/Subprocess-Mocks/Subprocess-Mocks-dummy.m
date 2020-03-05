f6ca82d21d6b9cb957277de4db4cca6fa384a0b9
Cyrus Ingraham <cyrus.ingraham@jamf.com>
2020-03-05 09:00:34 -0600

    Removed example project

--- Example/Pods/Target Support Files/Subprocess-Mocks/Subprocess-Mocks-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Subprocess_Mocks : NSObject
-@end
-@implementation PodsDummy_Subprocess_Mocks
-@end
--- Example/Pods/Target Support Files/Subprocess/Subprocess-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Subprocess : NSObject
-@end
-@implementation PodsDummy_Subprocess
-@end
