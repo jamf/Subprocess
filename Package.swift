acb5c1373fefab399f8c564422397e7ac00d2f7c
Cyrus Ingraham <cyrus.ingraham@jamf.com>
2020-05-11 19:28:48 -0500

    Added dynamic linking options

--- Package.swift
+++ Package.swift
@@ -6,13 +6,50 @@ let package = Package(
     name: "Subprocess",
     platforms: [ .macOS(.v10_12) ],
     products: [
-        .library(name: "Subprocess", targets: [ "Subprocess" ]),
-        .library(name: "SubprocessMocks", targets: [ "SubprocessMocks" ])
+        .library(
+            name: "Subprocess",
+            type: .static,
+            targets: [ "Subprocess" ]
+        ),
+        .library(
+            name: "SubprocessMocks",
+            type: .static,
+            targets: [ "SubprocessMocks" ]
+        ),
+        .library(
+            name: "SubprocessDylib",
+            type: .dynamic,
+            targets: [ "Subprocess" ]
+        ),
+        .library(
+            name: "SubprocessMocksDylib",
+            type: .dynamic,
+            targets: [ "SubprocessMocks" ]
+        )
     ],
     targets: [
-        .target( name: "Subprocess", dependencies: []),
-        .target( name: "SubprocessMocks", dependencies: [ "Subprocess" ]),
-        .testTarget(name: "UnitTests", dependencies: [ "Subprocess", "SubprocessMocks" ]),
-        .testTarget(name: "SystemTests", dependencies: [ "Subprocess" ])
+        .target(
+            name: "Subprocess",
+            dependencies: []
+        ),
+        .target(
+            name: "SubprocessMocks",
+            dependencies: [
+                .target(name: "Subprocess")
+            ]
+        ),
+        .testTarget(
+            name: "UnitTests",
+            dependencies: [
+                .target(name: "Subprocess"),
+                .target(name: "SubprocessMocks")
+            ]
+        ),
+        .testTarget(
+            name: "SystemTests",
+            dependencies: [
+                .target(name: "Subprocess")
+            ]
+        )
     ]
 )
